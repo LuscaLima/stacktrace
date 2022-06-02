e7f3492076c9b5c2110804a71ff0a6d009d68d24
Lusca Lima <lusca.lima@outlook.com>
2022-06-01 21:11:55 -0300

    refactor: fix `AppCopyPast` component style

--- src/components/AppCopyPaste/index.vue
+++ src/components/AppCopyPaste/index.vue
@@ -9,7 +9,7 @@ const { copy } = useClipboard({ source: props.text })
 
 <template>
   <div
-    class="flex justify-between items-center w-fit px-4 py-2 border-[1px] border-slate-300 rounded bg-white"
+    class="inline-flex justify-between items-center w-fit px-3 py-2 border-[1px] border-slate-300 rounded bg-white"
   >
     <strong class="font-semibold mr-2">
       {{ text }}
