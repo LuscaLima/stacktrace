0de5ed63b9f6b69db287fc3dc215793d974bd37b
Lusca Lima <lusca.lima@outlook.com>
2022-06-03 12:27:29 -0300

    feat: add alias prop in `AppCopyPaste` component

--- src/components/AppCopyPaste/index.vue
+++ src/components/AppCopyPaste/index.vue
@@ -2,23 +2,32 @@
 import { useClipboard } from '@vueuse/core'
 import { PhCopy } from 'phosphor-vue'
 
-const props = defineProps<{ text: string }>()
+const emits = defineEmits<{ (e: 'copy'): void }>()
+const props = defineProps<{
+  copyText: string
+  alias?: string
+}>()
 
-const { copy } = useClipboard({ source: props.text })
+const { copy } = useClipboard({ source: props.copyText })
+
+function handleCopy() {
+  copy()
+  emits('copy')
+}
 </script>
 
 <template>
   <div
     class="inline-flex justify-between items-center w-fit px-3 py-2 border-[1px] border-slate-300 rounded bg-white"
   >
-    <strong class="font-semibold mr-2">
-      {{ text }}
+    <strong class="font-semibold mr-2 text-slate-700">
+      {{ alias || copyText }}
     </strong>
-    <div
-      class="flex justify-center items-center ml-2 text-slate-600 text-2xl cursor-pointer"
-      @click="() => copy()"
+    <button
+      class="flex justify-center items-center ml-2 text-slate-500 text-2xl cursor-pointer"
+      @click="handleCopy"
     >
       <PhCopy />
-    </div>
+    </button>
   </div>
 </template>
